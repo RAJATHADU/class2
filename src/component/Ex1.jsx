7a25090b9752e0d5325d83c8b8a901bdd7aeeb26
RAJATHADU <rajatharajju21116@gmail.com>
2023-12-12 13:32:21 +0530

    first commit

---
+++ src/component/Ex1.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Ex1(props) {
+    return(
+        <div>
+            <h3>States in Functional Components</h3>
+        </div>
+    )
+}
+
+export default Ex1
